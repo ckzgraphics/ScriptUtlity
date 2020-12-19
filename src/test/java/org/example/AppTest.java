b27c27ac82f68856383444832704d5c35e6e9057
Chins <5130990-ckzgraphics@users.noreply.gitlab.com>
2020-12-19 09:57:54 +0530

    testng updates

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,19 +2,15 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.sample.BaseWebDriver;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void initUtil(){
+        String propWorkbookName = System.getProperty("book");
+        String url = System.getProperty("url");
+        new BaseWebDriver(propWorkbookName, url);
     }
 }
